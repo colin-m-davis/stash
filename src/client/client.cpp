6ddfe7ce566dc58e5b9956a3f9ba3af467e79ca6
colindavis <colinmichaelsdavis@gmail.com>
2023-06-18 12:07:11 -0700

    client can submit multiple requests per connection

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -25,28 +25,30 @@ int main(int argc, char* argv[]) {
         boost::asio::connect(sock, resolver.resolve(host, port));
         
         std::cout << "connected.\n";
-        std::cout << "enter query: ";
-        char query_buff[Query::MAX_LENGTH];
-        std::cin.getline(query_buff, Query::MAX_LENGTH);
-        std::size_t query_length = std::strlen(query_buff);
-        boost::asio::write(
-            sock,
-            boost::asio::buffer(query_buff, query_length
-        ));
+        while (true) {
+            std::cout << "enter query:\n> ";
+            char query_buff[Query::MAX_LENGTH];
+            std::cin.getline(query_buff, Query::MAX_LENGTH);
+            std::size_t query_length = std::strlen(query_buff);
+            boost::asio::write(
+                sock,
+                boost::asio::buffer(query_buff, query_length
+            ));
 
-        char response_buff[Response::MAX_LENGTH];
-        boost::system::error_code error;
-        std::size_t response_length = sock.read_some(boost::asio::buffer(response_buff), error);
+            char response_buff[Response::MAX_LENGTH];
+            boost::system::error_code error;
+            std::size_t response_length = sock.read_some(boost::asio::buffer(response_buff), error);
 
-        if (error == boost::asio::error::eof) {
-            std::cerr << "unexpected EOF, exiting\n";
-            return 1;
-        } else if (error) {
-            throw boost::system::system_error(error);
+            if (error == boost::asio::error::eof) {
+                std::cerr << "unexpected EOF, exiting\n";
+                return 1;
+            } else if (error) {
+                throw boost::system::system_error(error);
+            }
+
+            response_buff[response_length] = '\0';
+            std::cout << response_buff << '\n';
         }
-
-        response_buff[response_length] = '\0';
-        std::cout << "response received:\n" << response_buff << '\n';
     }
     catch (std::exception &e) {
         std::cerr << "exception: " << e.what() << '\n';
